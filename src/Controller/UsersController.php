cb5144c7370bdd855be3f2a5a8d8bd40a2f89e07
Jihoon <jih.seo23@livemolo.me>
2021-07-18 09:02:33 +0900

    add success join page(working)

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -51,7 +51,7 @@ class UsersController extends AppController
             if ($this->Users->save($user)) {
                 $this->Flash->success(__('The user has been saved.'));
 
-                return $this->redirect(['action' => 'index']);
+                return $this->redirect(['action' => 'successJoin']);
             }
             $this->Flash->error(__('The user could not be saved. Please, try again.'));
         }
@@ -100,6 +100,11 @@ class UsersController extends AppController
         }
 
         return $this->redirect(['action' => 'index']);
+    }
+
+    public function successJoin()
+    {
+        
     }
 
     public function naverJoin()
