432d5073913e3206b4e82b8fed6dbe1bf0ee07da
Jihoon <jih.seo23@livemolo.me>
2021-07-19 09:20:06 +0900

    add file upload example

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -161,7 +161,51 @@ class PagesController extends AppController
             }
             throw new NotFoundException();
         }
-    }
 
- 
+        //파일 업로드 예제
+
+        if($this->getRequest()->getData(‘type’)==1){ //이력서 직접등록
+            parse_str($this->getRequest()->getData(‘queryString’), $output); //serilaize된 querystring 파싱
+            // echo(“<pre>“);print_r($output);exit;
+            $index = strpos(strrev($_FILES[‘file’][‘name’]) , strrev(‘.’));
+            $expen = strtolower(substr( $_FILES[‘file’][‘name’] , ($index * -1) )); //파일 확장자 분리
+            $name =  date(“YmdHis”) . “_” . $_FILES[‘file’][‘name’];
+            //do the actual uploading of the file. First arg is the tmp name, second arg is
+            //where we are putting it
+            if(move_uploaded_file( $_FILES[‘file’][‘tmp_name’], WWW_ROOT . ‘upload/proposal/’ . $name)){
+                //데이터 저장
+                $applicant->name = $output[‘name’];
+                $applicant->yyyymmdd = $output[‘yyyymmdd’];
+                $applicant->hp = $output[‘hp’];
+                $applicant->email = $output[‘email’];
+                $applicant->worknet = $output[‘worknet’];
+                $applicant->company_id_1st = $output[‘company_id_1st’];
+                $applicant->company_name_1st = $this->getRequest()->getData(‘company_name_1st’);
+                // $applicant->selected_day_1st = $output[‘selected_day_1st’];
+                $applicant->company_id_2st = $output[‘company_id_2st’];
+                $applicant->company_name_2st = $this->getRequest()->getData(‘company_name_2st’);
+                // $applicant->selected_day_2st = $output[‘selected_day_2st’];
+                $applicant->interview_type = $output[‘interview_type’];
+                $applicant->interview_place = $this->getRequest()->getData(‘interview_place’);
+                $applicant->proposal_type = $output[‘proposal_type’];
+                $applicant->resume_image_path = ‘upload/proposal/’ . $name;
+                $applicant->reg_date = date("Y-m-d H:i:s");
+                if ($applicantTable->save($applicant)) {
+                    // The $article entity contains the id now
+                    $conn->commit();
+                    echo json_encode(array("data"=>true,'msg'=>'등록이 완료되었습니다.'));exit;
+                }
+                else{
+                    //오류
+                    $conn->rollback();
+                    echo json_encode(array("data"=>false,'msg'=>'등록에 실패하였습니다.'));exit;
+                }
+            }
+            else{
+                //오류
+                $conn->rollback();
+                echo json_encode(array("data"=>false,‘msg’=>'등록에 실패하였습니다.'));exit;
+            }
+        }
+    }
 }
