0f9d646445d5bb08452560034c6b11a5df7e64ad
springsandy <s2018w26@e-mirim.hs.kr>
2021-08-26 17:26:31 +0900

    modify users edit

--- templates/Users/edit.php
+++ templates/Users/edit.php
@@ -4,6 +4,8 @@
  * @var \App\Model\Entity\User $user
  */
 ?>
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script type="text/javascript" src="https://service.iamport.kr/js/iamport.payment-1.1.5.js"></script>
 <div class="row">
     <aside class="column">
         <div class="side-nav">
@@ -19,7 +21,7 @@
                 <?php
                     echo $this->Form->control('email', ['label' => '*이메일', 'readonly' => 'readonly']);
                     echo $this->Form->control('password', ['label' => '*비밀번호']);
-                    echo $this->Form->control('password_check', ['label' => '*비밀번호 확인']);
+                    echo $this->Form->control('password_check', ['label' => '*비밀번호 확인', 'type' => 'password']);
                     echo $this->Form->control('name', ['label' => '*이름']);
                     echo $this->Form->control('hp', ['label' => '휴대전화 번호', 'type' => 'tel']);
                     if ($user->hp_cert == 0) {
@@ -43,8 +45,17 @@
                 ?>
             </fieldset>
             <?= $this->Form->button(__('취소'), ['action' => 'index']) ?>
-            <?= $this->Form->button(__('저장')) ?>
+            <?= $this->Form->button(__('저장'), ['id' => 'save', 'name' => 'save']) ?>
             <?= $this->Form->end() ?>
         </div>
     </div>
 </div>
+<script>
+    $('button[name=save]').on('click', function() {
+        var password = $('input[name=password]').val();
+        var password_check = $('input[name=password_check]').val();
+        if (password != password_check) {
+            alert('비밀번호가 틀립니다.');
+        }
+    });
+</script>
