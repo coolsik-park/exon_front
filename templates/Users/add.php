3e7e9c95f76476055dd2b5f286af68e1d4826530
Jihoon <jih.seo23@gmail.com>
2021-09-17 13:14:41 +0900

    html users/add

--- templates/Users/add.php
+++ templates/Users/add.php
@@ -15,38 +15,6 @@ $kakao_redirectURI = urlencode("http://121.126.223.225:8765/users/kakaoJoin");
 $kakao_apiURL = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=".$kakao_client_id."&redirect_uri=".$kakao_redirectURI
 ?>
 
-<!-- <div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading">
-                <?= __('Actions') ?>
-            </h4>
-            <?= $this->Html->link(__('List Users'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="users form content">
-            <?= $this->Form->create($user) ?>
-            <fieldset>
-                <legend>
-                    <?= __('Add User') ?>
-                </legend>
-                <?php
-                    echo $this->Form->control('email');
-                    echo $this->Form->control('password');
-                    echo $this->Form->control('confirm_password',['type' => 'password']);
-                    echo $this->Form->control('name');
-                    echo $this->Form->control('hp');
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
-        </div>
-    </div>
-    <a href="<?php echo $naver_apiURL ?>"><img src="/img/naver/네이버아이디로로그인.png" /></a><br>
-    <a href="<?php echo $kakao_apiURL ?>"><img src="/img/kakao/ko/카카오아이디로로그인.png" /></a>
-</div> -->
-
 <div id="container">
     <div class="log-wrap">
         <div class="log-step-wrap">
