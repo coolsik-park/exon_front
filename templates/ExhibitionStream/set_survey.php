35686b8b6d1c62f79d2b4e592b5e4f62cc76e8e0
Jihoon <jih.seo23@gmail.com>
2021-10-05 11:25:23 +0900

    html exhibition-stream/set-survey

--- templates/ExhibitionStream/set_survey.php
+++ templates/ExhibitionStream/set_survey.php
@@ -1,4 +1,4 @@
-<div class="column-responsive column-80">
+<!-- <div class="column-responsive column-80">
     <div class="exhibitionStream form content"> 
         <?= $this->Form->create() ?>
         <?php
@@ -34,10 +34,43 @@
         <?= $this->Form->end() ?>
         <?= $this->Form->button('확인', ['id' => 'add']) ?>
     </div>
+</div> -->
+
+<div class="webinar-cont1">
+    <h3 class="sr-only">설문</h3>
+    <div class="webinar-cont-ty1">
+        <div class="webinar-cont-ty1-body">                                    
+            <div class="poll-item-wrap">
+                <p class="poll-item-b-tit">노출시킬 설문을 선택해 주세요</p>
+                <?php if (!empty($groupedSurveys)) : ?>
+                <?php $i = 0; ?>
+                <?php foreach ($groupedSurveys as $groupedSurvey) : ?>
+                <div class="poll-item-b">
+                    <span class="chk-dsg"><input type="checkbox" id="chk<?=$i?>" name="checked" value="<?=$groupedSurvey->id?>"><label for="chk<?=$i?>"><?=$groupedSurvey->text?></label></span>
+                    <div class="b-list">
+                    <?php $count = count($groupedSurvey['child_exhibition_survey']); ?>
+                    <?php if ($count != 0) : ?>      
+                    <?php for ($i = 0; $i < $count; $i++) : ?>
+                        <p><?=$i+1?>. <?=$groupedSurvey['child_exhibition_survey'][$i]['text']?></p>
+                    <?php endfor; ?>
+                    <?php endif; ?> 
+                    </div>
+                </div>
+                <?php $i++; ?>
+                <?php endforeach; ?>
+                <?php endif; ?>                             
+            </div>
+        </div>
+        <div class="webinar-cont-ty1-btm">
+            <div class="poll-submit">                                        
+                <button type="button" id="save" class="btn-ty4 redbg">확인</button>
+            </div>
+        </div>
+    </div>                            
 </div>
 
 <script>
-    $("#add").click(function () {
+    $("#save").click(function () {
         var lists = [];
         $("input[name='checked']:checked").each(function(i){ 
             lists.push($(this).val());
