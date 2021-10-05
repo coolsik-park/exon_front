35c4dc3d77b54beacc4f5a267599dbf34846bdf0
Jihoon <jih.seo23@gmail.com>
2021-10-05 11:01:27 +0900

    html exhibition-stream/set-speaker

--- src/Controller/ExhibitionStreamController.php
+++ src/Controller/ExhibitionStreamController.php
@@ -229,59 +229,88 @@ class ExhibitionStreamController extends AppController
 
     public function setSpeaker ($id = null)
     {
-        $connection = ConnectionManager::get('default');
-        $connection->begin();
-
         $ExhibitionSpeaker = $this->getTableLocator()->get('ExhibitionSpeaker');
-    
-        $displayes = $ExhibitionSpeaker->find('all')->where(['exhibition_id' => $id])->toArray();
         
         if ($this->request->is('post')) {
-            
-            $entities = $ExhibitionSpeaker->newEntities($this->request->getData());
-            $i = 0;
-            foreach ($entities as $entity) {
-                if ($result = $ExhibitionSpeaker->save($entity)) {
-                    
-                    $img = $this->request->getData('image' . $i);
-                    $i++;
-                    $imgName = $img->getClientFilename();
 
-                    if ($imgName != '') {
+            if ($this->request->getData('action') == 'image') {
+                $img = $this->request->getData()['image'];
+                $imgName = $img->getClientFilename();
+                $index = strpos(strrev($imgName), strrev('.'));
+                $expen = strtolower(substr($imgName, ($index * -1)));
+                $path = 'upload' . DS . 'speaker_temp' . DS . date("Y") . DS . date("m");
+                
+                if ($expen == 'jpeg' || $expen == 'jpg' || $expen == 'png') {
+                    
+                    if (!file_exists(WWW_ROOT . $path)) {
+                        $oldMask = umask(0);
+                        mkdir(WWW_ROOT . $path, 0777, true);
+                        chmod(WWW_ROOT . $path, 0777);
+                        umask($oldMask);
+                    }
+    
+                    // $imgName = $this->Auth->user('id') . "_main." . $expen;
+                    $destination = WWW_ROOT . $path . DS . $imgName;
+                    $img->moveTo($destination);
+
+                    $response = $this->response->withType('json')->withStringBody(json_encode(['status' => 'success', 'path' => $path, 'imgName' => $imgName]));
+                    return $response;
+
+                }else {
+                    $response = $this->response->withType('json')->withStringBody(json_encode(['status' => '이미지 확장자명을 확인해주세요.',]));
+                    return $response;
+                }
+            } else {
+                $connection = ConnectionManager::get('default');
+                $connection->begin();
+
+                $names = $this->request->getData('names');
+                $images = $this->request->getData('images');
+                $count = count($names);
+                
+                for ($i = 0; $i < $count; $i++) {
+                    $exhibitionSpeaker = $ExhibitionSpeaker->newEmptyEntity();
+                    $exhibitionSpeaker->name = $names[$i];
+                    $exhibitionSpeaker->exhibition_id = $id;
+                    
+                    if ($result = $ExhibitionSpeaker->save($exhibitionSpeaker)) {
+                        $img = $images[$i];
+                        $imgName = $img->getClientFilename();
                         $index = strpos(strrev($imgName), strrev('.'));
                         $expen = strtolower(substr($imgName, ($index * -1)));
                         $path = 'upload' . DS . 'speaker' . DS . date("Y") . DS . date("m");
 
-                        if ($expen == 'jpeg' || $expen == 'jpg' || $expen == 'png') {
-                                
-                            if (!file_exists(WWW_ROOT . $path)) {
-                                $oldMask = umask(0);
-                                mkdir(WWW_ROOT . $path, 0777, true);
-                                chmod(WWW_ROOT . $path, 0777);
-                                umask($oldMask);
-                            }
+                        if (!file_exists(WWW_ROOT . $path)) {
+                            $oldMask = umask(0);
+                            mkdir(WWW_ROOT . $path, 0777, true);
+                            chmod(WWW_ROOT . $path, 0777);
+                            umask($oldMask);
+                        }
+                        $imgName = $result->id . "_speaker." . $expen;
+                        $destination = WWW_ROOT . $path . DS . $imgName;
 
-                            $imgName = $result->id . "_speaker." . $expen;
-                            $destination = WWW_ROOT . $path . DS . $imgName;
-                            
-                            if ($connection->update('exhibition_speaker', ['image_path' => $path, 'image_name' => $imgName], ['id' => $result->id])) {
-                                $img->moveTo($destination);
-                            } else {
-                                $connection->rollback(); 
-                                $this->Flash->error(__('The exhibition speaker could not be saved.'));
-                            }
+                        if ($connection->update('exhibition_speaker', ['image_path' => $path, 'image_name' => $imgName], ['id' => $result->id])) {
+                            $img->moveTo($destination);
+
                         } else {
                             $connection->rollback(); 
-                            $this->Flash->error(__('The exhibition speaker could not be saved.'));
+                            $response = $this->response->withType('json')->withStringBody(json_encode(['status' => 'fail']));
+                            return $response;
                         }
+
+                    } else {
+                        $connection->rollback();
+                        $response = $this->response->withType('json')->withStringBody(json_encode(['status' => 'fail']));
+                        return $response;
                     }
                 }
+                $connection->commit();
+                $response = $this->response->withType('json')->withStringBody(json_encode(['status' => 'success']));
+                return $response;
             }
-            $connection->commit();
-            $this->Flash->success(__('The exhibition speaker has been saved.'));
-            return $this->redirect(['action' => 'setExhibitionStream', $id]); 
         }
-        $this->set(compact('displayes', 'id'));
+        $displays = $ExhibitionSpeaker->find('all')->where(['exhibition_id' => $id]);
+        $this->set(compact('id', 'displays'));
     }
 
     public function setAnswered ($id = null)
--- templates/ExhibitionStream/set_speaker.php
+++ templates/ExhibitionStream/set_speaker.php
@@ -1,100 +1,114 @@
-<div class="column-responsive column-80">
-    <div class="exhibitionStream form content"> 
-        <?= $this->Form->create(null, ['enctype' => 'multipart/form-data']) ?>
-        <table class="table table-bordered" id="dynamic_field">  
-            <tr>  
-                <td><?php echo $this->Form->control('image0', ['type' => 'file']); ?></td>
-                <td><?php echo $this->Form->control('0.exhibition_id', ['type' => 'hidden', 'value' => $id]); ?></td>          
-                <td><?php echo $this->Form->control('0.name'); ?></td>
-                 					 
-                <td><button type="button" name="add" id="add" class="btn btn-outline-primary btn-flat">add<i class="fas fa-plus"></i></button></td>  
-            </tr>  
-        </table> 
-
-        <?= $this->Form->button(__('Submit')) ?>
-        <?= $this->Form->end() ?>
+<form id="speakerForm" enctype="multipart/form-data">
+    <div class="wb-in-tab-cont">
+        <div id="wbCon1" class="on">
+            <h4 class="sr-only">연사자</h4>
+            <ul id="speakerUl" class="ph-items">
+                <li>
+                    <div class="ph-item-btn">
+                        <button id="add" type="button" class="ph-item-add">추가</button>
+                    </div>                                                    
+                </li>
+            </ul>
+        </div>
+        <!-- <div id="wbCon2" class="on">
+            <h4 class="sr-only">연사자</h4>
+            <ul class="ph-items">
+                <?php $displayIndex = 0; ?>
+                <?php foreach ($displays as $display) : ?>
+                <li id="speaker_<?=$displayIndex?>">
+                    <div class="ph-item">
+                        <label for="<?=$displayIndex?>"><div class="photo"><img id="speakerImg_<?=$displayIndex?>" src="/<?=$display->image_path?>/<?=$display->image_name?>"></div>
+                        <input type="file" id="<?=$displayIndex?>" name="image[]" style="display:none">
+                        <div class="tx"><input type="text" name="name[]" style="width:90px; height:24px;" value="<?=$display->name?>"></div>
+                        <button type="button" class="btn-del" onclick="deleteSpeaker(<?=$speakerIndex?>)">삭제</button>
+                    </div>
+                </li>
+                <?php $displayIndex++; ?>
+                <?php endforeach; ?>
+            </ul>
+        </div> -->
     </div>
-</div>
-
-<div class="wb-in-tab-cont">
-    <div id="wbCon1" class="on">
-        <h4 class="sr-only">연사자</h4>
-        <ul class="ph-items">
-            <li>
-                <div class="ph-item">
-                    <div class="photo"></div>
-                    <div class="tx">전체</div>
-                    <button type="button" class="btn-del">삭제</button>
-                </div>
-            </li>
-            <li>
-                <div class="ph-item">
-                    <div class="photo"></div>
-                    <div class="tx">유비 교수</div>
-                    <button type="button" class="btn-del">삭제</button>
-                </div>
-            </li>
-            <li>
-                <div class="ph-item">
-                    <div class="photo"></div>
-                    <div class="tx">관우 교수</div>
-                    <button type="button" class="btn-del">삭제</button>
-                </div>
-            </li>
-            <li>
-                <div class="ph-item">
-                    <div class="photo"></div>
-                    <div class="tx">장비 교수</div>
-                    <button type="button" class="btn-del">삭제</button>
-                </div>
-            </li>
-            <li>
-                <div class="ph-item">
-                    <div class="photo"></div>
-                    <div class="tx">장비 교수</div>
-                    <button type="button" class="btn-del">삭제</button>
-                </div>
-            </li>
-            <li>
-                <div class="ph-item">
-                    <div class="photo"></div>
-                    <div class="tx">장비 교수</div>
-                    <button type="button" class="btn-del">삭제</button>
-                </div>
-            </li>
-            <li>
-                <div class="ph-item">
-                    <div class="photo"></div>
-                    <div class="tx">장비 교수</div>
-                    <button type="button" class="btn-del">삭제</button>
-                </div>
-            </li>
-            <li>
-                <div class="ph-item-btn">
-                    <button type="button" class="ph-item-add">추가</button>
-                </div>                                                    
-            </li>
-        </ul>
+    <div class="webinar-cont-ty1-btm">
+        <div class="poll-submit">                                        
+            <button type="button" id="save" class="btn-ty4 redbg">저장</button>
+        </div>
     </div>
-</div>
+</form>
+
+
+
 <script>  
+    //발표자 추가
+    <?php $speakerIndex = 0; ?>
+    $("#add").click(function () {
+        var html = '';
+        html += '<li id="speaker_<?=$speakerIndex?>"">';
+        html += '   <div class="ph-item">';
+        html += '       <label for="<?=$speakerIndex?>"><div class="photo"><img id="speakerImg_<?=$speakerIndex?>"></div>';
+        html += '       <input type="file" id="<?=$speakerIndex?>" name="image[]" style="display:none">';
+        html += '       <div class="tx"><input type="text" name="name[]" style="width:90px; height:24px;"></div>';
+        html += '       <button type="button" class="btn-del" onclick="deleteSpeaker(<?=$speakerIndex?>)">삭제</button>';
+        html += '   </div>';
+        html += '</li>'
+        $("#speakerUl").append(html);
+        <?php $speakerIndex++; ?>
+    });
 
-$(document).ready(function(){
-    var i=1;  
-    $('#add').click(function(){  
-        $('#dynamic_field').append(
-        '<tr id="row'+i+'">'+
-        '<td><label for="image'+i+'">image'+i+'</label><input id="image'+i+'" name="image'+i+'" label="abc" type="file"></td>'+ 
-        '<td><input id="'+i+'-exhibition_id" name="'+i+'[exhibition_id]" type="hidden" value="<?=$id?>"></td>'+
-        '<td><label for="'+i+'-name">Name</label><input id="'+i+'-name" name="'+i+'[name]" type="text"></td>'+
-        '<td><button type="button" name="remove" id="'+i+'" class="btn btn-outline-danger btn-flat btn_remove">remove<i class="fas fa-times"></i></button></td>'+
-        '</tr>'
-        );  
-        i++;  
-    });  
-    $(document).on('click', '.btn_remove', function(){  
-        var button_id = $(this).attr("id");   
-        $('#row'+button_id+'').remove();  
-    });  
-});  
+    //발표자 삭제
+    function deleteSpeaker(index) {
+        $("#speaker_" + index).remove();
+        <?php $speakerIndex--; ?>
+    };
+
+    //사진 추가
+    $(document).on("change", "input[name='image[]']", function() {
+        var id = $(this).attr("id");
+        var formData = new FormData();
+        var image = document.getElementById(id).files;
+        formData.append('image', image[0]);
+        formData.append('action', 'image');
+        
+        jQuery.ajax({
+            url: '/exhibition-stream/set-speaker/<?=$id?>',
+            processData: false,
+            contentType: false,
+            cache: false,
+            data: formData,
+            type: 'POST',
+        }).done(function (data) {
+            if (data.status == 'success') {
+                $("#speakerImg_" + id).attr("src", "/" + data.path + "/" + data.imgName);
+            } else {
+                alert('이미지 등록에 실패하였습니다. 잠시 후 다시 시도해 주세요.');
+            }
+        });
+    });
+
+    //저장
+    $("#save").click(function () {
+        var formData = new FormData();
+        var images = document.getElementsByName('image[]');
+        for (var i = 0; i < images.length; i++) {
+            formData.append('images[]', images[i].files[0]);
+        }
+        var names = document.getElementsByName('name[]');
+        for (var i = 0; i < names.length; i++) {
+            formData.append('names[]', names[i].value);
+        }
+        jQuery.ajax({
+            url: '/exhibition-stream/set-speaker/<?=$id?>',
+            processData: false,
+            contentType: false,
+            cache: false,
+            data: formData,
+            type: 'POST',
+        }).done(function (data) {
+            if (data.status == 'success') {
+                alert('저장되었습니다.');
+                $("#questionContent").load("/exhibition-stream/set-speaker/" + <?= $id ?>);
+            } else {
+                alert('저장에 실패하였습니다. 잠시 후 다시 시도해주세요.');
+            }
+        });   
+    });
 </script>
