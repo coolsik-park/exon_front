fc7e6d8df0c6f8ab3c73d883857d4f7c6302cb0a
Jihoon <jih.seo23@gmail.com>
2021-10-08 15:13:58 +0900

    html exhibition/email certification

---
+++ templates/Users/certification.php
@@ -0,0 +1,31 @@
+<div id="container">
+    <div class="join-ok-wrap static">
+        <div class="join-ok-sect1">
+            <h3>회원가입이 완료되었습니다</h3>
+            <p class="tx1">축하드립니다, 이제부터 EXON 서비스를 자유롭게 즐기실 수 있습니다.</p>
+            <a href="/" class="btn-big-cir">가입완료</a>
+        </div>
+        <div class="join-ok-sect2">
+            <div class="js1">
+                <h4>본인인증</h4>
+                <p class="tx1">본인인증이 완료가 되면 편리하게 웨비나를 즐기실 수 있습니다.</p>
+            </div>
+            <div class="js2">
+                <a id="emailButton" class="btn-md" style="cursor:pointer;">이메일 인증</a>
+                <a id="hpButton" class="btn-md" style="cursor:pointer;">휴대폰 인증</a>
+            </div>
+        </div>            
+    </div>
+</div>
+
+<script>
+    $('#hpButton').on('click', function() {
+        var user_id = '<?=$id?>';
+        window.open('/users/sendSmsCertification/'+user_id+'', '휴대전화 인증', 'width=800px,height=500px,left=800px,top=300px');
+    });
+
+    $('#emailButton').on('click', function() {
+        var user_id = '<?=$id?>';
+        window.open('/users/sendEmailCertification/'+user_id+'', '이메일 인증', 'width=800px,height=500px,left=800px,top=300px');
+    });
+</script>
