13dae6389d0ee9a27dba975d541e08d018579eca
springsandy <s2018w26@e-mirim.hs.kr>
2021-10-12 22:02:56 +0900

    merge

--- templates/Boards/faqs_by_category.php
+++ templates/Boards/faqs_by_category.php
@@ -1,10 +1,196 @@
-<?php
-/**
- * @var \App\View\AppView $this
- * @var \App\Model\Entity\User[]|\Cake\Collection\CollectionInterface $users
- */
-?>
-<div class="userquestion index content">
+<div id="container">
+    <div class="contents static">
+        <div class="section-cs2">
+            <h3 class="s-hty1">카테고리 별 질문</h3>
+            <div class="cs-cate">
+                <ul>
+                    <li class="active">
+                        <button type="button" id="all">
+                            <span>전체</span>
+                            <span><?= $this->Paginator->counter(__('{{count}}')) ?></span>
+                        </button>
+                    </li>
+                    <li>
+                        <button type="button" id="user">
+                            <span>회원</span>
+                            <span>21</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button type="button" id="refund">
+                            <span>환불</span>
+                            <span>14</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button type="button" id="pay">
+                            <span>결제</span>
+                            <span>15</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button type="button" id="exhibitionParticipation">
+                            <span>행사 참여</span>
+                            <span>11</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button type="button" id="exhibitionAdd">
+                            <span>행사 개설</span>
+                            <span>110</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button type="button" id="webinar">
+                            <span>웨비나</span>
+                            <span>11</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button type="button" id="besides">
+                            <span>기타</span>
+                            <span>18</span>
+                        </button>
+                    </li>
+                </ul>
+            </div>
+            <h4 class="s-hty3" id="s-hty3">전체</h4>
+            <?php foreach ($faqs as $faq): ?>
+                <div id="board-lists">
+                    <ul class="board-lists">
+                        <li>
+                            <button type="button" class="b-tit"><?= $faq->title ?></button>
+                        </li>
+                    </ul>
+                </div>
+            <?php endforeach; ?>
+            <div class="paginator" >
+                <ul class="pagination">
+                    <?= $this->Paginator->prev('< ' . __('이전')) ?>
+                    <?= $this->Paginator->numbers() ?>
+                    <?= $this->Paginator->next(__('다음') . ' >') ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>    
+    $('#all').on('click', function() {
+        $.ajax({
+            url: '/boards/faqs-by-category',
+            method: 'POST',
+            type: 'json',
+            data: {
+                FaqCategoryId: 'null'
+            }
+        }).done(function(data) {
+            $('#s-hty3').replaceWith('<h4 class="s-hty3" id="s-hty3">전체</h4>');
+            $('#board-lists').load(location.href+" #board-lists");
+        });
+    });
+
+    $('#user').on('click', function() {
+        $.ajax({
+            url: '/boards/faqs-by-category',
+            method: 'POST',
+            type: 'json',
+            data: {
+                FaqCategoryId: 1
+            }
+        }).done(function(data) {
+            $('#s-hty3').replaceWith('<h4 class="s-hty3" id="s-hty3">회원</h4>');
+            $('#board-lists').load(location.href+" #board-lists");
+        });
+    });
+    
+    $('#refund').on('click', function() {
+        $.ajax({
+            url: '/boards/faqs-by-category',
+            method: 'POST',
+            type: 'json',
+            data: {
+                FaqCategoryId: 2
+            }
+        }).done(function(data) {
+            $('#s-hty3').replaceWith('<h4 class="s-hty3" id="s-hty3">환불</h4>');
+            $('#board-lists').load(location.href+" #board-lists");
+        });
+    });
+
+    $('#pay').on('click', function() {
+        $.ajax({
+            url: '/boards/faqs-by-category',
+            method: 'POST',
+            type: 'json',
+            data: {
+                FaqCategoryId: 3
+            }
+        }).done(function(data) {
+            $('#s-hty3').replaceWith('<h4 class="s-hty3" id="s-hty3">결제</h4>');
+            $('#board-lists').load(location.href+" #board-lists");
+        });
+    });
+    
+    $('#exhibitionParticipation').on('click', function() {
+        $.ajax({
+            url: '/boards/faqs-by-category',
+            method: 'POST',
+            type: 'json',
+            data: {
+                FaqCategoryId: 4
+            }
+        }).done(function(data) {
+            $('#s-hty3').replaceWith('<h4 class="s-hty3" id="s-hty3">행사 참여</h4>');
+            $('#board-lists').load(location.href+" #board-lists");
+        });
+    });
+
+    $('#exhibitionAdd').on('click', function() {
+        $.ajax({
+            url: '/boards/faqs-by-category',
+            method: 'POST',
+            type: 'json',
+            data: {
+                FaqCategoryId: 5
+            }
+        }).done(function(data) {
+            $('#s-hty3').replaceWith('<h4 class="s-hty3" id="s-hty3">행사 개설</h4>');
+            $('#board-lists').load(location.href+" #board-lists");
+        });
+    });
+    
+    $('#webinar').on('click', function() {
+        $.ajax({
+            url: '/boards/faqs-by-category',
+            method: 'POST',
+            type: 'json',
+            data: {
+                FaqCategoryId: 6
+            }
+        }).done(function(data) {
+            $('#s-hty3').replaceWith('<h4 class="s-hty3" id="s-hty3">웨비나</h4>');
+            $('#board-lists').load(location.href+" #board-lists");
+        });
+    });
+
+    $('#besides').on('click', function() {
+        $.ajax({
+            url: '/boards/faqs-by-category',
+            method: 'POST',
+            type: 'json',
+            data: {
+                FaqCategoryId: 7
+            }
+        }).done(function(data) {
+            $('#s-hty3').replaceWith('<h4 class="s-hty3" id="s-hty3">기타</h4>');
+            $('#board-lists').load(location.href+" #board-lists");
+        });
+    });
+</script>
+
+
+<!-- <div class="userquestion index content">
     <h3><?= __('FAQs by Category') ?></h3>
     <div>
         <?= $this->Html->link(__('전체'), ['action' => 'faqsByCategory']) ?>
@@ -38,4 +224,4 @@
         </ul>
         <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
     </div>
-</div>
+</div> -->
