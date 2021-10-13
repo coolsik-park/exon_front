bf5f708ba8e396ea9cd16317d4d5ce6e1dec53bc
Jihoon <jih.seo23@gmail.com>
2021-10-13 17:30:55 +0900

    add path front url

--- config/paths.php
+++ config/paths.php
@@ -92,3 +92,5 @@ define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'vendor' . DS . 'cakephp' . DS . 'c
  */
 define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
 define('CAKE', CORE_PATH . 'src' . DS);
+
+define('FRONT_URL', 'http://121.126.223.225:8765');
