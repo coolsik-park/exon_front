91b1af15352c6d2faa4707e94e3dd9a67a8add4c
springsandy <s2018w26@e-mirim.hs.kr>
2021-10-13 18:00:45 +0900

    html boards/add

--- templates/Boards/faqs_by_category.php
+++ templates/Boards/faqs_by_category.php
@@ -5,7 +5,7 @@
                 <ul class="s-tabs2">
                     <li class="active"><a href="">자주 하는 질문</a></li>
                     <li><a href="/boards/notice">공지사항</a></li>
-                    <li><a href="">문의하기</a></li>
+                    <li><a href="/boards/add">문의하기</a></li>
                 </ul>
             </div>
             <div class="section-cs1">
