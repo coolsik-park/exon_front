9bdca0bed91a3ed0de246703d822ff6e1c67960f
springsandy <s2018w26@e-mirim.hs.kr>
2021-10-20 11:08:44 +0900

    html modify exhibition/view

--- templates/Exhibition/view.php
+++ templates/Exhibition/view.php
@@ -106,7 +106,7 @@
                             </li>
                         </ul>
                         <div class="btns">
-                            <?php if ($exhibitionUsers == 0): ?>
+                            <?php if ($exhibitionUsers == null): ?>
                                 <div class="group" id="group">
                                     <?= $this->Form->select('', $groups, ['id' => 'group']) ?>                                   
                                     <span class="tx" id="spanGroup"></span>
@@ -114,15 +114,15 @@
                                 <a href="/exhibitionUsers/add/<?= $exhibition->id ?>" class="btn-join">참가 신청</a>
                             <?php 
                                 else: 
-                                    if($exhibitionUsers[0]->status == '4'):
+                                    if($exhibitionUsers[0]->status != '4'):
                             ?>
-                                        <a href="/exhibition-stream/watch-exhibition-stream/<?= $exhibition->id ?>" class="btn-join">웨비나 접속</a>
-                            <?php   else: ?>
                                         <div class="group" id="group">
                                             <?= $this->Form->select('', $groups, ['id' => 'group']) ?>                                   
                                             <span class="tx" id="spanGroup"></span>
                                         </div>
                                         <a href="/exhibitionUsers/add/<?= $exhibition->id ?>" class="btn-join">참가 신청</a>
+                            <?php   else: ?>
+                                        <a href="/exhibition-stream/watch-exhibition-stream/<?= $exhibition->id ?>" class="btn-join">웨비나 접속</a>
                             <?php 
                                     endif;
                                 endif; 
