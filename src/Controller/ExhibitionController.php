19c6a08ceb5d6263bd479c38cacabde47fa9156c
springsandy <s2018w26@e-mirim.hs.kr>
2021-11-03 10:32:10 +0900

    html modify exhibition/view

--- src/Controller/ExhibitionController.php
+++ src/Controller/ExhibitionController.php
@@ -71,7 +71,7 @@ class ExhibitionController extends AppController
         
         if ($this->Auth->user('id') != null) {
             $exhibitionUsers_table = TableRegistry::get('ExhibitionUsers');
-            $exhibitionUsers = $exhibitionUsers_table->find('all')->where(['users_id' => $this->Auth->user('id')])->toArray();
+            $exhibitionUsers = $exhibitionUsers_table->find('all')->where(['exhibition_id' => $id, 'users_id' => $this->Auth->user('id')])->toArray();
         } else {
             $exhibitionUsers = null;
         }
