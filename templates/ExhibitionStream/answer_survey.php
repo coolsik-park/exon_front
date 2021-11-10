9171f6ea38390669a86c536553e5dbf24c7f8870
Jihoon <jih.seo23@gmail.com>
2021-11-10 09:44:17 +0900

    modify exhibition-stream/tabs

--- templates/ExhibitionStream/answer_survey.php
+++ templates/ExhibitionStream/answer_survey.php
@@ -1,18 +1,18 @@
 <style>
-  .poll-item-wrap::-webkit-scrollbar {
-    width: 10px;
-  }
-  .poll-item-wrap::-webkit-scrollbar-thumb {
-    background-color: #2f3542;
-    border-radius: 10px;
-    background-clip: padding-box;
-    border: 2px solid transparent;
-  }
-  .poll-item-wrap::-webkit-scrollbar-track {
-    background-color: grey;
-    border-radius: 10px;
-    box-shadow: inset 0px 0px 5px white;
-  }
+    .poll-item-wrap::-webkit-scrollbar {
+        width: 10px;
+    }
+    .poll-item-wrap::-webkit-scrollbar-thumb {
+        background-color: #2f3542;
+        border-radius: 10px;
+        background-clip: padding-box;
+        border: 2px solid transparent;
+    }
+    .poll-item-wrap::-webkit-scrollbar-track {
+        background-color: grey;
+        border-radius: 10px;
+        box-shadow: inset 0px 0px 5px white;
+    }
 </style>
 
 <div class="webinar-cont1">
