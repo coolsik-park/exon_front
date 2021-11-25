c9b835a74d829e9f2c29e953340513d601fcbeeb
Jihoon <jih.seo23@gmail.com>
2021-11-25 14:26:21 +0900

    modify exhibition/search

--- templates/Exhibition/search.php
+++ templates/Exhibition/search.php
@@ -142,6 +142,7 @@
             <div class="paginator">
                 <ul class="pagination">
                     <?= $this->Paginator->prev("< ") ?>
+                    <?= $this->Paginator->numbers() ?>
                     <?= $this->Paginator->next(" >") ?>
                 </ul>
             </div>
