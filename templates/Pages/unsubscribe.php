1d4811c22e38aceb8d44201fa9f2c45d244b2c8a
Jihoon <jih.seo23@gmail.com>
2021-11-29 11:19:09 +0900

    add pages/unsubscribe

---
+++ templates/Pages/unsubscribe.php
@@ -0,0 +1,74 @@
+<style>
+    h1 {
+        font-size: 25px;
+        margin: 26px 0px 35px 0px;
+        font-weight: bold;
+    }
+    h2 {
+        color: #5491DA;
+        font-size: 18px;
+        margin: 20px 0px;
+    }
+    h3 {
+        margin: 20px 0px;
+        font-size: 18px;
+        font-weight: bold;
+    }
+    h4 {
+        margin: 15px 10px 12px 0px;
+    }
+    p {
+        margin: 8px 0px;
+        line-height: 28px;
+    }
+    .textBox {
+        display: flex;
+    }
+    .title {
+        font-size: 28px;
+        margin: 36px 0px;
+    }
+    @media  screen and (max-width: 768px) {
+        .title {
+            margin: 66px 0px;
+            font-size: 24px;
+        }
+        p {
+            font-size: 16px;
+        }
+        h4 {
+            font-size: 16px;
+        }
+    }
+</style>
+<div id="container">
+    <div class="contents static">
+        <div class="section-my">
+            <h3 class="title">이메일 주소 무단수집 거부</h3>
+            <h3>개요</h3>
+            <div class="textBox">
+                <P>본 웹사이트(http://exon.live)에 게시된 이메일 주소가 전자우편 수집프로그램이나 그 밖의 기술적 장치를 이용하여 무단으로 수집되는 것을 거부하며, 
+                    이를 위반 시 정보통신망법에 의해 형사 처벌됨을 유념하시기 바랍니다.
+                </p>
+            </div>
+            <h3>이메일집단수집거부 안내</h3>
+            <div class="textBox">
+                <P>이메일의 주체에게 사전동의를 받지 않고 본 홈페이지에 게시된 이메일 주소를 수집하여 영리목적의 광고를 하는 행위는 정보통신망법 제 50조제1항에 
+                    의하여 3천만원 이하의 과태료가 부과될 수 있습니다.
+                </p>
+            </div>
+            <h3>정보통신망법 제50조(영리목적의 광고성 정보 전송 제한) </h3>
+            <div class="textBox">
+                <h4>①</h4>
+                <P>누구든지 전자적 전송매체를 이용하여 영리목적의 광고성 정보를 전송하려면 그 수신자의 명시적인 사전 동의를 받아야 한다.
+                </p>
+            </div>
+            <h2>제50조(영리목적의 광고성 정보 전송 제한) </h2>
+            <div class="textBox">
+                <h4>①</h4>
+                <P>누구든지 전자적 전송매체를 이용하여 영리목적의 광고성 정보를 전송하려면 그 수신자의 명시적인 사전 동의를 받아야 한다.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
