724088c468cfedc4e5fc0abd67022897febd4989
jinyeong03 <jinyeong2808@naver.com>
2022-02-09 10:42:28 +0900

    modify Exhibition/manager_person second

--- templates/Exhibition/manager_person.php
+++ templates/Exhibition/manager_person.php
@@ -15,8 +15,10 @@
     .board-sh {
         margin: 0 auto;
     }
-    .modal-dialog {
-        margin: 6px auto;
+    @media  screen and (max-width: 1600px) {
+        .modal-dialog {
+            margin: 6px auto;
+        }
     }
 </style>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
