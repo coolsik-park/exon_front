d4edb88ec13a240d806c289b8dea961d269bffdf
Jihoon <jih.seo23@gmail.com>
2022-03-31 11:16:09 +0900

    modify live end

--- templates/ExhibitionStream/edit_exhibition_stream.php
+++ templates/ExhibitionStream/edit_exhibition_stream.php
@@ -511,9 +511,6 @@
                     method: 'DELETE',
                     type: 'json',
                     data: jsonData,
-                    error: function (data) {
-                        alert("오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
-                    }
                 });
             }
         });
