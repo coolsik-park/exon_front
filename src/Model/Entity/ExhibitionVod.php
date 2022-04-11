396c77a85a17051112675223d8e4b74d4dec0620
Jihoon <jih.seo23@gmail.com>
2022-04-11 16:39:02 +0900

    add vod pages

--- src/Model/Entity/ExhibitionVod.php
+++ src/Model/Entity/ExhibitionVod.php
@@ -9,12 +9,16 @@ use Cake\ORM\Entity;
  * ExhibitionVod Entity
  *
  * @property int $id
+ * @property string $type
  * @property int $exhibition_id
  * @property string $title
+ * @property int|null $parent_id
  * @property \Cake\I18n\FrozenTime $created
  * @property \Cake\I18n\FrozenTime $modified
  *
- * @property \App\Model\Entity\Exhibition $exhibition
+ * @property \App\Model\Entity\ExhibitionStream $exhibition_stream
+ * @property \App\Model\Entity\ExhibitionVod $parent_exhibition_vod
+ * @property \App\Model\Entity\ExhibitionVod[] $child_exhibition_vod
  */
 class ExhibitionVod extends Entity
 {
@@ -28,10 +32,14 @@ class ExhibitionVod extends Entity
      * @var array
      */
     protected $_accessible = [
+        'type' => true,
         'exhibition_id' => true,
         'title' => true,
+        'parent_id' => true,
         'created' => true,
         'modified' => true,
-        'exhibition' => true,
+        'exhibition_stream' => true,
+        'parent_exhibition_vod' => true,
+        'child_exhibition_vod' => true,
     ];
 }
