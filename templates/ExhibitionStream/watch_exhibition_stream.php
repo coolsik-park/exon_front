c99da65f1dc500905ffe18995efd8ac8d759a127
Jihoon <jih.seo23@gmail.com>
2022-05-25 18:43:08 +0900

    modify chat

--- templates/ExhibitionStream/watch_exhibition_stream.php
+++ templates/ExhibitionStream/watch_exhibition_stream.php
@@ -362,7 +362,7 @@
         });
 
         $("#li9").click(function () {
-            $(".webinar-tab-body").load("/exhibition-stream-chat-log/chat/" + <?= $exhibitionStream[0]['exhibition_id'] ?> + "/" + now);
+            $(".webinar-tab-body").load("/exhibition-stream-chat-log/chat/" + <?= $exhibitionStream[0]['exhibition_id'] ?> + "/" + now + "/" + <?=$exhibition_users_id?>);
             $("#li0").attr("class", "");
             $("#li1").attr("class", "");
             $("#li2").attr("class", "");
