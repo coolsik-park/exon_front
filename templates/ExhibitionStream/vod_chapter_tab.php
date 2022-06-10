99b184f7201a85db2a1dd3363d4cb9c02159b81b
jinyeong03 <jinyeong2808@naver.com>
2022-06-10 10:43:37 +0900

    modify set_exhibition_vod.php vod_chapter_tab.php

--- templates/ExhibitionStream/vod_chapter_tab.php
+++ templates/ExhibitionStream/vod_chapter_tab.php
@@ -1,9 +1,27 @@
+<style>
+    .chapter {
+        position: relative;
+    }
+    .arrow--vod__1 {
+        position: absolute;
+        right: 0px;
+        top: 0px;
+    }
+    .table-type .tr-row {
+        height: 40px;
+    }
+</style>
+
+
 <div class="webinar-cont2">
     <div class="table-type table-type2" style="margin:20px;">      
     <?php foreach ($exhibitionVod as $list) : ?>              
         <div class="tr-row">
             <div class="chapter">
-                <p><?=$list['title']?></p>
+                <p><?=$list['title']?></p> 
+                <a style="" class="c arrow--vod__1" name="<?=$list['id']?>">
+                    <img id="arrow--vod" class="chapter-icon arrow--vod" src="/img/arrow-down-sign-to-navigate.png">
+                </a>
             </div>
             <ul class="vod-ul">
             <?php foreach ($list['child_exhibition_vod'] as $vod) : ?>
