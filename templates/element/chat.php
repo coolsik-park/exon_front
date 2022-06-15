e0cb212c108ca34789008d371dc2b9b8e33e2735
Jihoon <jih.seo23@gmail.com>
2022-06-15 17:19:45 +0900

    fix chat

--- templates/element/chat.php
+++ templates/element/chat.php
@@ -3,7 +3,7 @@
     <div style="display:flex;">
         <span class='chat-time' style='display:flex;'><?php echo date("H:i A", strtotime("+9 hours", strtotime($list['created'])));?></span>
         <b class='user-name' style="display:flex; height:24px; width:fit-content;"><?php echo $list['user_name']; ?></b>
-        <?php echo '<b style="display:flex;">'.$list['message'].'</b>'; ?>
+        <?php echo '<b style="display:flex; margin-top:2px;">'.$list['message'].'</b>'; ?>
     </div>
 </div>
 <?php endforeach; ?>
