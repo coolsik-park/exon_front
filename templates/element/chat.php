a2d940c200c5b8b85eb699484c211278f1ffce38
Jihoon <jih.seo23@gmail.com>
2022-06-15 10:53:52 +0900

    modify chat

--- templates/element/chat.php
+++ templates/element/chat.php
@@ -1,7 +1,9 @@
 <?php foreach($message as $index =>$list): ?>
 <div class='msgln' style="line-height:120%">
 <span class='chat-time'><?php echo date("g:i A", strtotime("+9 hours", strtotime($list['created']))); ?></span>
-<b class='user-name'><?php echo $list['user_name']; ?></b>
-<?php echo '<b>'.$list['message'].'</b>'; ?>
+    <div style="display:flex;">
+        <b class='user-name' style="display:flex; height:24px;"><?php echo $list['user_name']; ?></b>
+        <?php echo '<b style="display:flex;">'.$list['message'].'</b>'; ?>
+    </div>
 </div>
 <?php endforeach; ?>
