1aab8f4dea71c7c40fa65c14f40bbd5710e72079
jinyeong03 <jinyeong2808@naver.com>
2022-06-20 15:01:25 +0900

    modify exhibitionStream/tmp_chapter.php

--- templates/ExhibitionStream/tmp_chapter.php
+++ templates/ExhibitionStream/tmp_chapter.php
@@ -10,7 +10,7 @@
     .vod-ul {
         margin: 2% 0 2% 3%;
         font-size: 1.5rem;
-        border-bottom: 1px solid black;
+      
         height: 80px;
     }
 
@@ -56,28 +56,28 @@
 
     .vod--li__title {
         margin-left: 25px;
-        position: absolute;
-        top: 20px;
-        width: 85%;
+        top: 0px;
         height: 50px;
         cursor: default;
+        position: absolute;
+        font-size: 24px;
+        font-weight: 500;
     }
 
     .chapter--title__char {
         font-size: 16px;
-        top: 25px;
-        left: 0px;
         position: absolute;
+        top: 5px;
     }
     .vod--li__date {
-        margin-left: 25px;
+        line-height: 25px;
     }
     .vod--li__description {
-        margin-left: 25px;
+        position: relative;
+        /* margin-left: 25px; */
     }
     .vod--info__div {
         margin-top: 80px;
-        display: none;
     }
     a {
         cursor: pointer;
@@ -90,61 +90,113 @@
         position: absolute;
         left: 1045px;
     }
+    .table-type2 {
+        padding-bottom: 100px;
+    }
+    .vod--row__div {
+        margin-bottom: 50px;
+    }
+    .tr-row__vod {
+        border-bottom: 1px solid black;
+        display: flex;
+        width: 100%;
+        position: relative;
+        padding-bottom: 40px;
+        font-size: 18px;
+    }
+    .vod--img__div {
+        width: 30%;
+        height: 200px;
+    }
+    .vod--date__div {
+        width: 30%;
+        height: 200px;
+        padding: 0px 50px;
+        display: flex;
+        flex-direction: column;
+    }
+    .vod--dec__div {
+        width: 40%;
+        height: 200px;
+        padding: 0px 20px;
+        display: flex;
+        flex-direction: column;
+        word-break:keep-all;
+    }   
+    .vod--img {
+        width: 100%;
+        height: 200px;
+    }
+    .vod--dec__char {
+        /* position: absolute; */
+    }
+    .vod--title__span {
+        margin-bottom: 10px;
+        font-weight: 500;
+    }
 </style>
 
-<div id="container">
+
     <div class="contents">
         <div class="section-webinar3">
             <div class="webinar-cont">
                 <div class="section-my">
                     <h3 class="s-hty1"><?= $exhibition->title ?></h3>
                     <div class="table-type table-type2">
-                        <div class="tr-row">
+                        
                             <?php $i = 0; ?>
                             <?php foreach ($tmp_exhibitions as $tmp_exhibition) : ?>
-                                <ul class="vod-ul">
-                                    <li class="vod-li vod--title__div">
+                                <div class="vod--row__div">
+                                    <div class="tr-row__vod">
                                         <span class="chapter--title__char">○</span>
                                         <span class="vod--li__title"><?= $tmp_exhibition['title'] ?></span>
                                         <?php if ($tmp_exhibition_streams[$i]['live_started'] == null) : ?>
-                                            <button type="button" id="copy_stream_key" class="btn-ty2 bor vod-time">라이브 시청</button>
+                                        <button type="button" id="copy_stream_key" class="btn-ty2 bor vod-time">라이브 시청</button>
                                         <?php else : ?>
-                                            <a class="vod--a" href="/exhibition-stream/watch-exhibition-stream/<?= $tmp_exhibition['id'] ?>/<?= $exhibitionUsers[$i]['id'] ?>/<?= $cert ?>" style="">
-                                                <button type="button" id="copy_stream_key" class="btn-ty2 red vod-time"> 라이브 시청 </button>
-                                            </a>
+                                        <a class="vod--a" href="/exhibition-stream/watch-exhibition-stream/<?= $tmp_exhibition['id'] ?>/<?= $exhibitionUsers[$i]['id'] ?>/<?= $cert ?>" style="">
+                                            <button type="button" id="copy_stream_key" class="btn-ty2 red vod-time"> 라이브 시청 </button>
+                                        </a>
                                         <?php endif; ?>
-                                    </li>
-                                    <li class="vod-li vod--info__div">
-                                        <span class="vod--li__date"><?=$tmp_exhibition['sdate']?> ~ <?=$tmp_exhibition['edate']?></span><br><br>
-                                        <span class="vod--li__description"><?=$tmp_exhibition['description']?></span>
-                                    </li>
-                                </ul>
+
+                                            <div class="vod-li vod--img__div">
+                                                <img src="/img/@item7.jpg" class="vod--img" />
+                                            </div>
+                                            <div class="vod-li vod--date__div">
+                                                <span class="vod--title__span">일시</span>
+                                                <span class="vod--li__date"><?=$tmp_exhibition['sdate']?> ~ <br> <?=$tmp_exhibition['edate']?></span><br><br>
+                                            </div>
+                                            <div class="vod-li vod--dec__div">
+                                                <span class="vod--title__span">- 행사 내용</span>
+                                                <span class="vod--dec__char">*</sapn>
+                                                <span class="vod--li__description"><?=$tmp_exhibition['description']?></span>
+                                            </div>
+                                    </div>
+                                </div>
                             <?php $i++; ?>
                             <?php endforeach; ?>
-                        </div>
+                        
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 <script>
-    $(document).on("click", ".vod--li__title", function(){
-        if( $(this).parent().next().hasClass('visible')){
-            // $(this).parent().next().slideUp();
-            $(this).parent().next().css("display", "none");
-            $(this).parent().parent().css("height", "80px");
-            $(this).parent().next().removeClass('visible');
-            $(this).css("border", "none");
-        }
-        else {
-            // $(this).parent().next().slideDown();
-            $(this).parent().next().css("display", "block");
-            $(this).parent().parent().css("height", "240px");
-            $(this).parent().next().addClass('visible');
-            $(this).css("border-bottom", "1px solid lightgrey");
-        }
-    });
+    // $(document).on("click", ".vod--li__title", function(){
+    //     if( $(this).parent().next().hasClass('visible')){
+    //         // $(this).parent().next().slideUp();
+    //         $(this).parent().next().css("display", "none");
+    //         $(this).parent().parent().css("height", "80px");
+    //         $(this).parent().next().removeClass('visible');
+    //         $(this).css("border", "none");
+    //     }
+    //     else {
+    //         // $(this).parent().next().slideDown();
+    //         $(this).parent().next().css("display", "block");
+    //         $(this).parent().parent().css("height", "240px");
+    //         $(this).parent().next().addClass('visible');
+    //         $(this).css("border-bottom", "1px solid lightgrey");
+    //     }
+    // });
 
 </script>
